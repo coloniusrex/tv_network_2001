87a36c8a9f0f749cb1e0c8f7041f97045db8b91c
Colin Alexander <colintalex@gmail.com>
2020-02-11 10:57:09 -0700

    Adds completed methods for Network class. actors_by_show method functioning, needs testing re-work.

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,31 @@
+require './lib/show'
+require './lib/character'
 
+class Network
+  attr_reader :name
+  attr_accessor :shows
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    main = []
+    @shows.each do |show|
+      main.concat(show.characters)
+    end
+    main
+  end
+
+  def actors_by_show
+    shows = {}
+    @shows.each do |show|
+      shows[show] = show.actors
+    end
+    shows
+  end
+end
